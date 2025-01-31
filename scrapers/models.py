e06a409891e82395686e921d5a5c1978da937d0e
ljros <eliotrs@gmail.com>
2025-02-01 00:37:58 +0100

    added missing import

--- scrapers/models.py
+++ scrapers/models.py
@@ -1,4 +1,4 @@
-# from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime
 # from sqlalchemy.ext.declarative import declarative_base
 # from sqlalchemy.engine.url import URL
 from datetime import datetime
