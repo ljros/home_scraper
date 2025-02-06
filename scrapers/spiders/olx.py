576318b1960d5ab2fe10cf1ea7639a2343b97301
ljros <eliotrs@gmail.com>
2025-02-06 16:34:53 +0100

    added first not working draft of olxv2

--- scrapers/spiders/olx.py
+++ scrapers/spiders/olx.py
@@ -22,6 +22,63 @@ class OlxSpider(scrapy.Spider):
 
         results = []
 
+        parsed_state = self.extract_prerendered_state(script_content)
+        offers = self.extract_offers(parsed_state)
+        print(offers)
+        
+        
+    def extract_prerendered_state(script_content):
+        patterns = [
+            r'window\.__PRERENDERED_STATE__\s*=\s*(["\'])(.*?)\1\s*;'
+        ]
+        
+        for pattern in patterns:
+            match = re.search(pattern, script_content, re.DOTALL)
+            if match:
+                escaped_json = match.group(2)
+                
+                try:
+                    unescaped_json = escaped_json.replace('\\"', '"') \
+                                                .replace("\\'", '"') \
+                                                .replace('\\\\', '\\')
+                    
+                    parsed_data = json.loads(unescaped_json)
+                    
+                    return parsed_data
+                
+                except json.JSONDecodeError:
+                    try:
+                        # If direct parsing fails, try parsing the raw escaped string
+                        parsed_data = json.loads(escaped_json)
+                        return parsed_data
+                    except json.JSONDecodeError as e:
+                        print(f"Parsing error: {e}")
+                        print(f"Problematic string: {escaped_json}")
+        
+        raise ValueError("Could not find and parse configuration")
+
+    def extract_offers(json_data):
+        try:
+            ads_list = json_data.get('listing', {}).get('listing', {}).get('ads', []),
+
+            if ads_list:
+                return ads_list
+            
+            return []
+        
+        except Exception as e:
+            print(f"Error extracting ads: {e}")
+            return []
+
+ ###### to be removed if up works
+
+    def parse_old(self, response):
+
+        if response.status == 307:
+            return
+
+        results = []
+
         listings = response.css('div.listing-grid-container > div:nth-of-type(2) div')
         for listing in listings:
             # skip promoted listings
@@ -33,17 +90,17 @@ class OlxSpider(scrapy.Spider):
             image = listing.css('div > div > div:nth-of-type(1) > a > div > div > img::attr(src)').get()
             short_desc = listing.css('div > div > div:nth-of-type(2) > div > a > h4::text').get()
             price = listing.css('div > div > div:nth-of-type(2) > div > p::text').get()
-            address = listing.css('div > div > div:nth-of-type(2) > div:nth-of-type(3) > p::text').get()
+            _district = listing.css('div > div > div:nth-of-type(2) > div:nth-of-type(3) > p::text').get()
             details_per_m2 = listing.css('div > div > div:nth-of-type(2) > div:nth-of-type(3) > div > span::text').get()
 
-            if address is None:
+            if _district is None:
                 continue
             if "otodom.pl" in link:
                 continue # skip crosslisted listings for now as the ones on olx have less info
             else:
                 link = 'https://www.olx.pl' + link
             
-            district = address.split(' -')[0].split(', ')[1]
+            district = _district.split(' -')[0].split(', ')[1]
             surface = details_per_m2.split(' - ')[0]
             price_per_m = details_per_m2.split(' - ')[1]
 
@@ -63,7 +120,6 @@ class OlxSpider(scrapy.Spider):
             result['image'] = image
             result['short_desc'] = short_desc
             result['price'] = price
-            result['address'] = address
             result['district'] = district
             result['currency'] = currency
             result['surface'] = surface
