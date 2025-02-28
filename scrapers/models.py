6f0566e2eec608cc164081e167c0e6dcc820e8ef
ljros <eliotrs@gmail.com>
2025-02-28 13:52:09 +0100

    new version rollout taking information from cache

--- scrapers/models.py
+++ scrapers/models.py
@@ -1,14 +1,46 @@
 from sqlalchemy import Column, Integer, String, DateTime
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.engine.url import URL
 from datetime import datetime
 from shared.database import Base
 
-class ApartmentListing(Base):
-    __tablename__ = 'apartment_listings'
+# Define the SQLAlchemy model
+
+class OlxListing(Base):
+    __tablename__ = 'olx_listings'
 
     id = Column(Integer, primary_key=True)
-    # Modify these fields based on your spider's items
+
+    platform = Column(String(25))
+    title = Column(String(255))
+    description = Column(Text)
+    map_link = Column(Text)
+    link = Column(String(255), unique=True)
+    external_link = Column(String(255))
+    listing_created_time = Column(TIMESTAMPTZ)
+    listing_last_refresh_time = Column(TIMESTAMPTZ)
+    price_per_m = Column(Numeric(6, 2))
+    floor = Column(Integer)
+    furniture = Column(String(3))
+    market = Column(String(25))
+    builttype = Column(String(50))
+    surface = Column(Numeric(4, 2))
+    rooms = Column(String(25))
+    price = Column(Numeric(12, 2))
+    currency = Column(String(3))
+    negotiable = Column(Boolean)
+    city = Column(String(50))
+    district = Column(String(50))
+    username = Column(String(255))
+    is_business = Column(Boolean)
+    images = Column(Text)
+    image = Column(Text)
+
+
+
+class OtodomListing(Base):
+    __tablename__ = 'apartment_listings' #todo change
+
+    id = Column(Integer, primary_key=True)
+
     link = Column(String(255))
     image = Column(Text)
     price = Column(Numeric(12, 2))
--- scrapers/spiders/olx.py
+++ scrapers/spiders/olx.py
@@ -3,8 +3,9 @@ import scrapy
 import logging
 import json
 import re
+from word2number import w2n
 
-from ..items import HomeItems, yield_item_with_defaults
+from ..items import OlxListingItem, yield_item_with_defaults
 
 class OlxSpider(scrapy.Spider):
 
@@ -26,9 +27,43 @@ class OlxSpider(scrapy.Spider):
         script_content = response.css("#olx-init-config::text").get()
         parsed_state = self.extract_prerendered_state(script_content)
         offers = self.extract_offers(parsed_state)
-        print(json.dumps(offers, indent=4))
-        with open('olx.json', 'w') as f:
-            json.dump(offers, f)
+        
+        for offer in offers:
+            description = offer.get('description', {})
+            description = re.sub(r"<.*?>", " ", description)  # Remove HTML tags
+            description = re.sub(r"\s+", " ", description).strip()  # Normalize spaces
+            map_data = offer.get('map', {})
+            params = offer.get('params', {})
+            price_info = offer.get('price', {}).get('regularPrice', {})
+            location = offer.get('location', {})
+
+            result ['platform'] = 'olx'
+            result['title'] = offer.get('title')
+            result['description'] = description
+            result['map_link'] = f"https://www.openstreetmap.org/#map={map_data['zoom']}/{map_data['lat']}/{map_data['lon']}" if map_data else None
+            result['link'] = offer.get('url')
+            result['external_link'] = offer.get('externalUrl')
+            result['listing_created_time'] = offer.get('createdTime')
+            result['listing_last_refresh_time'] = offer.get('lastRefreshTime')
+            result['price_per_m'] = next((param["normalizedValue"] for param in data["params"] if param["key"] == "price_per_m"), None)
+            result['floor'] = next((param["value"] for param in data["params"] if param["key"] == "floor_select"), None)    
+            result['furniture'] = 1 if next((param["normalizedValue"] for param in data["params"] if param["key"] == "furniture"), None) == "yes" else 0
+            result['market'] = next((param["normalizedValue"] for param in data["params"] if param["key"] == "market"), None)
+            result['builttype'] = next((param["normalizedValue"] for param in data["params"] if param["key"] == "builttype"), None)
+            result['surface'] = next((param["normalizedValue"] for param in data["params"] if param["key"] == "m"), None)
+            result['rooms'] = w2n.word_to_num(next((param["normalizedValue"] for param in data["params"] if param["key"] == "rooms_select"), None))
+            result['price'] = price_info.get('value')
+            result['currency'] = price_info.get('currencyCode')
+            result['negotiable'] = price_info.get('negotiable')
+            result['city'] = location.get('cityName')
+            result['district'] = location.get('districtName')
+            result['username'] = offer.get('user', {}).get('name')
+            result['is_business'] = offer.get('isBusiness')
+            result['images'] = json.dumps(offer.get('photos', []))
+            result['image'] = offer.get('photos', [])[0] if offer.get('photos', []) else None
+
+        yield from yield_item_with_defaults(results, OlxListingItem)
+
         
         
     def extract_prerendered_state(self, script_content):
@@ -133,7 +168,7 @@ class OlxSpider(scrapy.Spider):
 
         logging.info(f"Found {len(results)} listings on page {response.url}")
         for result in results:
-            yield from yield_item_with_defaults(result)
+            yield from yield_item_with_defaults(result, OlxListingItem)
 
     def _errback_httpbin(self, failure):
         # log all failures
